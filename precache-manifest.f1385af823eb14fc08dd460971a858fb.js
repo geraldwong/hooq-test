e6e8af87b11cc654c40cce88f5ca7aa069a2a4a8
Gerald Wong <e0003102@u.nus.edu>
2019-03-15 01:46:24 +0800

    Updates

---
+++ precache-manifest.f1385af823eb14fc08dd460971a858fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0388987fa61fa454646109e83ebd50ad",
+    "url": "/hooq-test/static/media/logo.0388987f.png"
+  },
+  {
+    "revision": "b0e4dcdf9554859b4227ff24fa693ab8",
+    "url": "/hooq-test/static/media/image_template.b0e4dcdf.jpg"
+  },
+  {
+    "revision": "75398d4be09988a9fc3c",
+    "url": "/hooq-test/static/js/runtime~main.982c7949.js"
+  },
+  {
+    "revision": "8c49ec69d68295a9e44a",
+    "url": "/hooq-test/static/js/main.612b8337.chunk.js"
+  },
+  {
+    "revision": "f7068fd8c39407dc23a0",
+    "url": "/hooq-test/static/js/2.5abd9954.chunk.js"
+  },
+  {
+    "revision": "8c49ec69d68295a9e44a",
+    "url": "/hooq-test/static/css/main.c915a769.chunk.css"
+  },
+  {
+    "revision": "56a5554a01207f2d49f8d41488a7cb78",
+    "url": "/hooq-test/index.html"
+  }
+];
